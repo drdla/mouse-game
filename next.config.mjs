60615567946cbc6f6805150817aa757a59d17414
Dominik Rodler <dominik.rodler@gmail.com>
2024-04-19 11:29:02 +0200

    Deploy app to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/mouse-game',
+  output: 'export', // Enable static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
